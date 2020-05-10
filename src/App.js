a3fdeb2567fde05e27a83c7c922c9b6d6032b18d
Jing Jie <jj@hireplace.com>
2020-05-10 17:42:19 +0800

    updating app with shope page and preview component and item preview component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import "./App.css";
-import {Switch, Route} from 'react-router-dom'
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
-
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
-    <Switch>
-      <Route exact path='/' component={HomePage} />
-    </Switch>
-      <HomePage />
-     </div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
+    </div>
   );
 }
 
